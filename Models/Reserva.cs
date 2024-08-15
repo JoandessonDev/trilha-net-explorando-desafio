588bd6e1a592187518511b7eb74e2a8444b30872
Joandesson <joandesson.dev@gmail.com>
2024-08-15 19:19:42 -0300

    implementando as funcionalidades

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,3 +1,4 @@
+using IO;
 namespace DesafioProjetoHospedagem.Models
 {
     public class Reserva
@@ -25,6 +26,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new ArgumentExeption("Capacidade Insuficiente!");
             }
         }
 
@@ -37,7 +39,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Lenth(); 
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +47,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados X Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados > 10)
             {
-                valor = 0;
+                return valor * 0.9;
             }
 
             return valor;
